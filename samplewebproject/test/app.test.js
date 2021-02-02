3c7557db22c50944ad42d48109b32f35a8334504
Andy McG <mcgow_a@hotmail.com>
2021-02-02 12:13:47 +0000

    interim commit

--- samplewebproject/test/app.test.js
+++ samplewebproject/test/app.test.js
@@ -1,4 +1,4 @@
-const { italic } = require("chalk");
+const { chalk } = require("chalk");
 const render = require("../../render");
 
 const assert = require('assert');
@@ -9,4 +9,20 @@ it('has a text input', async () => {
     const input = dom.window.document.querySelector('input');
 
     assert(input);
+
+    it('shows a success message with a valid email', async () => {
+        const dom = await render('index.html');
+
+        const input = dom.window.document.querySelector('input');
+        input.value = 'andy@andy.com';
+
+        dom.window.document
+            .querySelector('form')
+            .dispatchEvent(new dom.window.Event('submit'));
+
+            const h1 = dom.window.document.querySelector('h1');
+
+            console.log('Contents of h1', h1.innerHTML);
+
+    })
 });
