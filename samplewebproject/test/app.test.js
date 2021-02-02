08374509ac136ad3418f4dd4a89a90edce310563
Andy McG <mcgow_a@hotmail.com>
2021-02-02 16:38:59 +0000

    interim commit

--- samplewebproject/test/app.test.js
+++ samplewebproject/test/app.test.js
@@ -1,4 +1,3 @@
-const { chalk } = require("chalk");
 const render = require("../../render");
 
 const assert = require('assert');
@@ -22,7 +21,23 @@ it('has a text input', async () => {
 
             const h1 = dom.window.document.querySelector('h1');
 
-            console.log('Contents of h1', h1.innerHTML);
+            assert.strictEqual(h1.innerHTML, 'Valid email');
 
-    })
+    });
+
+    it('shows a failure message with an invalid email', async () => {
+        const dom = await render('index.html');
+
+        const input = dom.window.document.querySelector('input');
+        input.value = 'blah';
+
+        dom.window.document
+            .querySelector('form')
+            .dispatchEvent(new dom.window.Event('submit'));
+
+            const h1 = dom.window.document.querySelector('h1');
+
+            assert.strictEqual(h1.innerHTML, 'Invalid email');
+
+    });
 });
